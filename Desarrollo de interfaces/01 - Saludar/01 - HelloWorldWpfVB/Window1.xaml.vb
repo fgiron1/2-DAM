92910a9cc4b35f2f3805612d0eef3074118610f5
Fernando <fernangd9@gmail.com>
2020-09-30 13:47:35 +0200

    Aplicaciones WPF p.2

--- Desarrollo de interfaces/01 - Saludar/01 - HelloWorldWpfVB/Window1.xaml.vb
+++ Desarrollo de interfaces/01 - Saludar/01 - HelloWorldWpfVB/Window1.xaml.vb
@@ -1,5 +1,11 @@
-﻿Public Class Window1
+﻿Imports _01___Saludar
+
+Public Class Window1
+
+
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+        Dim persona As New clsPersona()
 
     End Sub
 End Class
