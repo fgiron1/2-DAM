9d23e306cab712ec644e7bc07bb9c9d5a5adcddc
Fernando <fernangd9@gmail.com>
2020-09-30 13:34:11 +0200

    Aplicaciones WPF

---
+++ Desarrollo de interfaces/01 - HelloWorld/01 - HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01___HelloWorld
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Boto_ççç__ñ______ppnnBotón_saludo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click()
+        {
+
+        }
+    }
+}
---
+++ Desarrollo de interfaces/01 - Saludar/01 - Saludar/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___Saludar
+{
+    public class clsPersona
+    {
+        //Instance variables
+        private String name;
+
+        #region Propiedades
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        #endregion
+
+        #region Constructores
+        //Default constructor
+        public clsPersona()
+        {
+            this.name = "Foo";
+        }
+
+        //Parameterized constructor
+        public clsPersona(String name)
+        {
+            this.name = name;
+        }
+        #endregion
+
+
+
+        //Campo nombre, constructor por defecto y constructor con parámetros
+    }
+}
