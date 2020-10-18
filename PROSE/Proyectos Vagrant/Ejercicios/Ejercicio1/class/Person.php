c91eb2bc39d40ef5ed3c600ea7682d7dee3a210c
Fernando <fernangd9@gmail.com>
2020-10-18 21:33:18 +0200

    He preparado para trabajar php

---
+++ PROSE/Proyectos Vagrant/Ejercicios/Ejercicio1/class/Person.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Person
+{
+    private $edad;
+
+    public function __construct($edad){$this->edad = $edad;}
+
+    /*
+     *@return int
+     */
+    public function getEdad(){return $this->edad;}
+
+    /*
+     * @return void
+     */
+    public function setEdad($edad){$this->edad = $edad;}
+}
