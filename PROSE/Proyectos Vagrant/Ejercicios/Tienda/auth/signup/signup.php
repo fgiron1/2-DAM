828457233ac8fe91ee9d8170d3a48af3e1a2c02d
Fernando <fernangd9@gmail.com>
2020-10-27 19:42:37 +0100

    -

--- PROSE/Proyectos Vagrant/Ejercicios/Tienda/auth/signup/signup.php
+++ PROSE/Proyectos Vagrant/Ejercicios/Tienda/auth/signup/signup.php
@@ -1,1 +1,31 @@
 <?php
+
+//Checking if the data has been sent
+var_dump($_POST);
+
+if(!isset($_POST["inputName"],
+          $_POST["inputSurname"],
+          $_POST["inputBirthdate"],
+          $_POST["inputPassword"],
+          $_POST["inputRepeatPassword"]) &&
+          $_SERVER["REQUEST_METHOD"] != "POST"){
+    exit("Please complete the registration form");
+}
+
+//Checking whether the fields are empty or not
+if(empty($_POST["inputName"]) ||
+    empty($_POST["inputSurname"]) ||
+    empty($_POST["inputBirthdate"]) ||
+    empty($_POST["inputPassword"]) ||
+    empty($_POST["inputRepeatPassword"])){
+    exit("Please fill out all the fields");
+}
+
+if($_POST["inputPassword"] !== $_POST["inputRepeatPassword"]){
+    exit("Passwords don't match");
+}
+
+//TODO CREAR OBJETO USUARIO
+//TODO HACERLO CON SESIONES
+
+echo ("Heyyy you did it");
