ef491ecf2d3e974de32ec971ea606ed432038825
Fernando <fernangd9@gmail.com>
2020-11-03 19:31:07 +0100

    -

--- Acceso a datos/ApuestasDeportivas.sql
+++ Acceso a datos/ApuestasDeportivas.sql
@@ -9,12 +9,23 @@ GO
 -- HAY QUE EXPLICAR LA BASE DE DATOS; NO SE VA A PRESENTAR.
 -- TENEMOS QUE DECIDIR UN NIVEL DE ISOLACIÓN DE LAS TRANSACCIONES
 
+--UNA TRANSACCIÓN REPRESENTA LA REALIDAD DE UN MOVIMIENTO DE DINERO, POR LO QUE EL MOVIMIENTO DE DINERO DEBE IR ANTES QUE
+-- LA CREACIÓN DE LA TRANSACCIÓN. NO OBSTANTE, LAS TRANSACCIONES TIENEN AUTORIDAD EN CUANTO A QUE NOS AYUDAN A CONTROLAR LOS PAGOS
+-- POR LO QUE,  UN MOVIMIENTO QUE NO TENGA SU TRANSACCIÓN CORRESPONDENTE NO TIENE NINGÚN TIPO DE GARANTÍA DE SER UN MOVIMIENTO LEGAL.
+-- PODRÍA SER EL RESULTADO DE ALGUIEN QUE EFECTIVAMENTE NOS DEBE/DEBEMOS DINERO, O UN ERROR EN EL SISTEMA
+
 --COMENTARIO DE STACK OVERFLOW SOBRE EL NIVEL DE ISOLACIÓN DE LAS TRANSACCIONES EN APLICACIONES MONETARIAS
 --A financial application probably has many situations where one record is read and then based on that one or more records including the original one are altered. Every tim you encounter a situation like that, it is more important that you indicate the write intend on the initial read by requesting a write or update lock. If you do that within the same transaction that executes the writes later on you are not dependent on the isolation level at all. You also will reduce the likelihood of deadlocks.
 --In the end, the choice of isolation level really depends on your requirements. The type of application is not a good guide to pick an isolation level. Look at each requirement and see if the implementation requires a specific isolation level.
 
 --AL INSERTAR FILAS EN LA FECHAS QUE TIENEN CAMPOS CON FECHA Y HORA DE CREACIÓN, PONER EN LA DEFINICIÓN DE TABLA
 --QUE SE AUTOGENERE
+
+--TENEMOS QUE CONTROLAR EL FORMATO DEL CAMPO resultado EN APUESTAS, EN FUNCIÓN DEL TIPO DE APUESTA QUE ES. Ejemplo:
+--Debemos evitar tener una apuesta con el tipo de "cantidad de corners" cuyo resultado esperado sea "El recre de huelva"
+
+--Se debería de poner un trigger que cerrase los partidos que tienen una edad de más de 3 días
+--y un procedimiento almacenado que tomara la id del partido como parámetro
 
 --ACLARACIONES IMPORTANTES PARA LA EXPLICACIÓN EN LA DOCUMENTACIÓN:
 -- Distinguimos 4 tipos de movimientos de dinero en el balance del usuario: Las ganancias y las pérdidas
@@ -86,7 +97,7 @@ CREATE TABLE Apuestas (
     cuota decimal(4,2) NOT NULL,
 	--Helps track illegal bets (bets to a nonexiting or finished match, etc...)
 	fechaRealizacion datetime NOT NULL,
-    resultado int NOT NULL,
+    resultado int NULL,
 
     CONSTRAINT PKApuestas PRIMARY KEY (ID),
     CONSTRAINT FKTipo FOREIGN KEY (idTipo) REFERENCES TiposApuestas (ID),
@@ -157,55 +168,86 @@ END
 GO
 
 --TRIGGERS
+
+
+---------------------------------------------ESTE TRIGGER ES INNECESARIO----------------------------------------------------------------------------
 --El control sobre el dinero de la transacción es responsabilidad del procedimiento apostar. Tomando dicho valor como referencia,
 --si contradice a la cantidad apostada que expresa la entidad transacción, se eliminará la apuesta y se revocará la transacción
---(Se creará una transacción por el mismo valor pero en positivo, es decir, se le devuelve el dinero al usuario)
+--(Se creará una transacción por el mismo valor pero en positivo, es decir, se le devuelve el dinero al usuario).
+--ESTE TRIGGER A LO MEJOR ES INUTIL: En el proceso de creación de una apuesta, el dinero que se usa como cantidad
+--es exactamente el mismo que se ha usado para crear la transacción; ambos se hacen en el cuerpo del mismo procedimiento
+--usando la variable con el mismo valor. La utilidad de este método yace en que si por algún otro motivo extraño esos números
+--difieren, la base de dato reconozca esa diferencia. No obstante, es muy costoso para lo poco útil que es.
 
 --Puede que este triger se pudiera definir de forma más típica de base de datos
-GO
-CREATE OR ALTER TRIGGER apuestaIgualSustraccion
-ON Apuestas
-FOR INSERT
-AS
+--GO
+--CREATE OR ALTER TRIGGER apuestaIgualSustraccion
+--ON Apuestas
+--FOR INSERT
+--AS
 
---La cantidad de dinero apostada de la fila recién insertada
-DECLARE @cantidad smallmoney
-SELECT @cantidad = cantidad FROM inserted
+----La cantidad de dinero apostada de la fila recién insertada
+--DECLARE @cantidad smallmoney
+--SELECT @cantidad = cantidad FROM inserted
 
---El importe de la transaccion que le corresponde a la apuesta recién insertada
-DECLARE @importe smallmoney
-SELECT @importe = importe
-FROM Transacciones AS T
-WHERE T.idApuesta = inserted.idApuesta
+----El importe de la transaccion que le corresponde a la apuesta recién insertada
+--DECLARE @importe smallmoney
+--SELECT @importe = importe
+--FROM Transacciones AS T
+--WHERE T.idApuesta = inserted.idApuesta
 
-IF(@cantidad <> @importe)
-	BEGIN
-		--Se elimina la apuesta introducida
-		DELETE FROM Apuestas AS A
-		WHERE A.ID = inserted.ID
+--IF(@cantidad <> @importe)
+--	BEGIN
+--		--Se elimina la apuesta introducida
+--		DELETE FROM Apuestas AS A
+--		WHERE A.ID = inserted.ID
 
-		--Se crea una nueva transacción en la que se le devuelva el dinero al usuario
-		INSERT INTO Transacciones
+--		--Se crea una nueva transacción en la que se le devuelva el dinero al usuario
+--		INSERT INTO Transacciones
 
-	END
+--	END
 
 
-GO
+--GO-----------------------------------------------------------------------------------------------------------------------------------------------------
 
 GO
 CREATE OR ALTER TRIGGER superaMaximoApuestas
 GO
+
 GO
 CREATE OR ALTER TRIGGER dniEnListaNegra
-GO
-GO
-CREATE OR ALTER TRIGGER apuestaAbierta
 GO
 
 --STORED PROCEDURES
 --COMIENZA LOS PROCEDIMIENTOS CON BEGIN TRANSACTION
+GO
+CREATE OR ALTER PROCEDURE apostar(usuario, cantidad, partido)--Crea entidad apuesta PRIMERO y despues resta el dinero, y finalmente crea transacción (Usa sustraerSaldo)
+AS
+	--Estas tres comprobaciones se pueden hacer en un if y apañao
+	--Si el partido permite apuestas y
+	--si el usuario tiene saldo y
+	--si no ha superado el maximo de apuestas, se crea una apuesta por @cantidad y se almacena su id,
+	--inicializando el resultado previsto del partido a null
+	--Seguidamente se le sustrae al usuario el dinero.
+	--Por último, se crea una entidad Transaccion por el valor de @cantidad con el id previamente almacenado
+GO
 
-CREATE OR ALTER PROCEDURE apostar(usuario, cantidad, partido) --Crea entidad transaccion PRIMERO y despues apuesta (Usa sustraerSaldo)
+GO
 CREATE OR ALTER PROCEDURE anadirSaldo(usuario, cantidad) --Crea entidad transaccion
+AS
+
+GO
+
+GO
 CREATE OR ALTER PROCEDURE sustraerSaldo(usuario, cantidad) --Crea entidad transaccion
-CREATE OR ALTERPROCEDURE cambiarEstadoApuestas() --No crea ninguna entidad
+AS
+
+GO
+
+GO
+CREATE OR ALTER PROCEDURE cambiarEstadoApuestas
+AS
+	UPDATE Partidos
+	-- ~ Operator flips the bit
+	SET puedeApostar = ~puedeApostar
+GO
