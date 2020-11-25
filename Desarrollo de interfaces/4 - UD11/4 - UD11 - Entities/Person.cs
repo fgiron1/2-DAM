8c4af396d0a1f9a537f69e253dfd3fc326789d33
Fernando <fernangd9@gmail.com>
2020-11-26 00:10:47 +0100

    Ejercicio 4 UD11 DIN comenzado

---
+++ Desarrollo de interfaces/4 - UD11/4 - UD11 - Entities/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4___UD11___Entities
+{
+    public class Person
+
+    {
+        public int ID { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public byte[] ProfilePic { get; set; }
+
+        public Person(string firstName, string lastName, DateTime Birthdate, string PhoneNumber, string Email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Birthdate = Birthdate;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
+
+
+        }
+    }
+}
---
+++ Desarrollo de interfaces/4 - UD11/4 - UD11/ViewModels/PersonVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _4___UD11___Entities;
+
+namespace _4___UD11.ViewModels
+{
+    //Esta clase debe ser public pero da un error extraño
+      public class PersonVM : clsVMBase
+    {
+        public ObservableCollection<Person> PersonList { get; set; }
+        public ObservableCollection<Person> PersonListBy { get; set; }
+        public Person PersonFocus { get; set; }
+        public string SearchTextPerson { get; set; }
+
+        #region Commands
+        private DelegateCommand _commandUpdate;
+        public DelegateCommand CommandUpdate
+        {
+            get
+            {
+
+            }
+
+            set
+            {
+               
+            }
+        }
+
+        private DelegateCommand _commandDelete;
+        public DelegateCommand CommandDelete
+        {
+            get
+            {
+
+            }
+
+            set
+            {
+
+            }
+        }
+        private DelegateCommand _commandInsert;
+        public DelegateCommand CommandInsert
+        {
+            get
+            {
+
+            }
+
+            set
+            {
+
+            }
+        }
+        #endregion
+    }
+}
