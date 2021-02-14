1b352ae715a60e8fe5394f4e3521a21ffe688d8c
Fernando <fernangd9@gmail.com>
2021-02-14 14:35:29 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Desarrollo de interfaces/CRUD_Xamarin/CRUD_Xamarin_DAL/BaseURI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CRUD_Xamarin_DAL
+{
+    public class BaseURI
+    {
+        public string URI;
+
+        public static string GetBaseURI()
+        {
+            return Uri;
+        }
+
+    }
+}
---
+++ Desarrollo de interfaces/CRUD_Xamarin/CRUD_Xamarin_Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CRUD_Xamarin_Entities
+{
+    public class Class1
+    {
+    }
+}
