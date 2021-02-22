38f3c0c99bde9988f8a5b143f2e7a5877e4b82ef
Fernando <fernangd9@gmail.com>
2021-02-22 12:16:57 +0100

    ACDAT Necesitas instalar jdk 1.8 de hotspot en casa

---
+++ Acceso a datos/SAX_2_Bueno_Final/src/main/Apuesta.java
@@ -0,0 +1,84 @@
+package main;
+
+import java.util.Date;
+
+public class Apuesta {
+
+    int usuario;
+
+    int partido;
+    float cantidad;
+    Date fecha;
+    //Puede ser 1, X o 2
+    String resultado;
+    //boolean no nos permite diferenciar que el campo este vacío
+    //porque tanto false como true tienen significados. Usamos String
+    String overunder;
+    float diferencia;
+    int handicap;
+
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getPartido() {
+        return partido;
+    }
+
+    public void setPartido(int partido) {
+        this.partido = partido;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getOverunder() {
+        return overunder;
+    }
+
+    public void setOverunder(String overunder) {
+        this.overunder = overunder;
+    }
+
+    public float getDiferencia() {
+        return diferencia;
+    }
+
+    public void setDiferencia(float diferencia) {
+        this.diferencia = diferencia;
+    }
+
+    public int getHandicap() {
+        return handicap;
+    }
+
+    public void setHandicap(int handicap) {
+        this.handicap = handicap;
+    }
+}
---
+++ Acceso a datos/SAX_2_Bueno_Final/src/main/ConductorSAX.java
@@ -0,0 +1,11 @@
+package main;
+
+
+public class ConductorSAX {
+public static void main (String[] args){
+	String nombreArchivo = "src\\apuestas.xml";
+	PruebaSAX1 probando = new PruebaSAX1 (nombreArchivo);
+        probando.leerArchivo();
+}// Fin main
+
+} // Fin conductorSAX
